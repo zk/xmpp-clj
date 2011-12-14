5c7fa16b7faf2fed6599b0645bc7794c50925377
zkim <zachary.kim@gmail.com>
2011-12-13 17:52:44 -0700

    Bump to 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xmpp-clj "0.2.0-SNAPSHOT"
+(defproject xmpp-clj "0.2.0"
   :description "A lightweight clojure wrapper around the smack jabber/XMPP library"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [jivesoftware/smack "3.1.0"]])
